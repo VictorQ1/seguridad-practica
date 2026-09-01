9dc9901baa8289ae4cb25a73be96446dc373189a
VictorQ1 <vic.quistian@gmail.com>
2026-09-01 01:20:38 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def inicio():
+    return "<h1>Sistema de seguridad de cuatro capas</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
